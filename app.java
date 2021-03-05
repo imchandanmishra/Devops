1eb25522268c29889d1622800065020084303997
Chandan <imchandanmishra@hotmail.com>
2021-03-05 11:57:41 +0100

    first commit devops intro

---
+++ app.java
@@ -0,0 +1,9 @@
+public class app {
+	public static void main(String args[]){  
+		System.out.println("Hello folks..5 times devops Intro");
+		  	for (int i=1; i<=5;i++) {
+		     System.out.println(i+".Welcome to Devops World");  
+		    }  
+	}
+
+}
